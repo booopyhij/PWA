b63bffb3e085ab6a3e34dcd3b27c5f49328b11ad
booopyhij <mphillips2017@gmail.comgit config --global user.name booopyhijgit config --global user.email mphillips2017@gmail.com>
2023-10-17 00:52:22 -0400

    added comments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,8 +1,9 @@
 import { openDB } from 'idb';
-
+//start the DB
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
+        //if db exists do nothing. else create the Jate db
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
@@ -19,11 +20,13 @@ export const putDb = async (content) => {
     const jateDb = await openDB("jate", 1);
     //create transaction
     const tx = jateDb.transaction("jate", "readwrite");
-    //object store
+    //store an object
     const store = tx.objectStore("jate");
     //use method to pass in content
     const request = store.put({ value: content });
+    //async end of the function
     const result = await request;
+     // console.log to make sure the logic runs properly
   
     console.log("data stored successfully", result);
   };
@@ -38,13 +41,14 @@ export const putDb = async (content) => {
     const tx = jateDb.transaction("jate", "readonly");
     //object store
     const store = tx.objectStore("jate");
-    // .getAll() method to get all data in the database
+    // .getAll() method to get all data 
     const request = store.getAll();
+    //async end of the function
     const result = await request;
-  
+  // console.log to make sure the logic runs properly
     console.log("result.value", result);
   
     return result.value;
   };
-  
+
 initdb();
