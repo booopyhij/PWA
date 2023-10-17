6954dc38015bf43cf89dfae3b63380c2850c8a03
booopyhij <mphillips2017@gmail.comgit config --global user.name booopyhijgit config --global user.email mphillips2017@gmail.com>
2023-10-17 01:05:35 -0400

    added comments and formatted the index.js

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,14 +1,19 @@
+// importing workbox, editor.js the indexed db and css
 import { Workbox } from 'workbox-window';
 import Editor from './editor';
 import './database';
 import '../css/style.css';
 
+//creating the html 'main' a constant and setting it to empty
 const main = document.querySelector('#main');
 main.innerHTML = '';
 
+//function to create divs
 const loadSpinner = () => {
   const spinner = document.createElement('div');
+  console.log(spinner);
   spinner.classList.add('spinner');
+  //setting the innerhtml to div classes
   spinner.innerHTML = `
   <div class="loading-container">
   <div class="loading-spinner" />
@@ -17,8 +22,9 @@ const loadSpinner = () => {
   main.appendChild(spinner);
 };
 
+// creating a new editor
 const editor = new Editor();
-
+//if the editor is undefined, call the loadspinner function
 if (typeof editor === 'undefined') {
   loadSpinner();
 }
@@ -29,5 +35,6 @@ if ('serviceWorker' in navigator) {
   const workboxSW = new Workbox('/src-sw.js');
   workboxSW.register();
 } else {
+    //logging error so programmer can see the error
   console.error('Service workers are not supported in this browser.');
 }
