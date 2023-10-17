c340bef340c134117731f5cd08ed6c282ca7f519
booopyhij <mphillips2017@gmail.comgit config --global user.name booopyhijgit config --global user.email mphillips2017@gmail.com>
2023-10-17 01:09:45 -0400

    added comments and formatted the install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,18 +1,20 @@
+// setting the HTML element buttonInstall to a JS constant
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
+// nstalling the PWA
 window.addEventListener("beforeinstallprompt", (event) => {
+//preventing the browser default
     event.preventDefault();
     //store triggered events
     window.deferredPrompt = event;
-    //remove hidden button
+    //toggle to make the button hidden 
     butInstall.classList.toggle("hidden", false);
   });
   
   //click event handler on the `butInstall` element
   butInstall.addEventListener("click", async () => {
     const promptEvent = window.deferredPrompt;
-  
+  // if it is not the prompt event end the function
     if (!promptEvent) {
       return;
     }
@@ -20,7 +22,7 @@ window.addEventListener("beforeinstallprompt", (event) => {
     promptEvent.prompt();
     //deferred prompt can only be used once, we reset it here
     window.deferredPrompt = null;
-  
+  //toggle to make the button visible
     butInstall.classList.toggle("hidden", true);
   });
   
